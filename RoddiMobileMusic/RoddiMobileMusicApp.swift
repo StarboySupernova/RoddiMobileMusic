b851d5edd1650a2d723c715f4aef64402f3995cc
Simbarashe Dombodzvuku <supernovaonline@outlook.com>
2022-03-29 01:42:31 +0100

    Initial Commit

---
+++ RoddiMobileMusic/RoddiMobileMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoddiMobileMusicApp.swift
+//  RoddiMobileMusic
+//
+//  Created by user217235 on 3/29/22.
+//
+
+import SwiftUI
+
+@main
+struct RoddiMobileMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
